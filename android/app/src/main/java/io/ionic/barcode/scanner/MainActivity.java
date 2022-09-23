cea156962ca802e4a7d5d85cbc63d2400f4d3093
Damian Tarnawsky <damian@ionic.io>
2022-09-23 14:42:55 -0700

    Initial Commit

---
+++ android/app/src/main/java/io/ionic/barcode/scanner/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.barcode.scanner;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScanPreferences } from '../scan-preferences';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
+  public scan() {
+    const preferences: ScanPreferences =
+    {
+      preferFrontCamera: false, // iOS and Android
+      showFlipCameraButton: true, // iOS and Android
+      showTorchButton: true, // iOS and Android
+      torchOn: false, // Android, launch with the flashlight switched on (if available)
+      saveHistory: false, // Android, save scan history (default false)
+      prompt: 'Place a barcode inside the scan area', // Android
+      resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+      formats: 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
+      orientation: 'portrait', // Android only (portrait|landscape), default unset so it rotates with the device
+      disableAnimations: true, // iOS
+      disableSuccessBeep: false // iOS and Android
+    };
+
+    (window as any).cordova.plugins.barcodeScanner.scan(this.onScanned, this.onError, preferences);
+  }
+
+  private onScanned(result) {
+    alert(`Scanned: "${result.text}" format: ${result.format}`);
+  }
+
+  private onError(error) {
+    alert(`Scanning failed: ${error}`);
+  }
 }
